aca908eac02b0c1f3c6b8685abb3797a428d979e
JD <gtx2@protonmail.com>
2019-03-19 21:40:49 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementsByClassName('container')[0].addEventListener('click',function(e){
+    
+    if(e.target.matches('a')){
+         const paragraphs = document.querySelectorAll('.container p'); 
+        
+        for(let i=0;i<paragraphs.length;i++){
+            paragraphs[i].classList.remove('visible');
+        } 
+        e.target.nextElementSibling.classList.toggle('visible');
+    
+    };
+    
+    });
+
+
